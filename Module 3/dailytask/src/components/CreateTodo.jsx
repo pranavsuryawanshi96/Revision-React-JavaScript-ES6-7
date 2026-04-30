86381741c9d645c7021e269a66eb2d71b0af59a9
pranavsuryawanshi96 <pranavsuryawanshi61@gmail.com>
2026-04-30 21:35:30 +0530

    dynamic js

--- Module 3/dailytask/src/components/CreateTodo.jsx
+++ Module 3/dailytask/src/components/CreateTodo.jsx
@@ -2,8 +2,14 @@ import React, { useState } from "react";
 
 const CreateTodo = () => {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("patil");
+  const task = ["taks1", "task2", "task3", "task4"];
+  
   const handleClick = (e) => {
     setCount(count + 1);
+  };
+  const handleChange = (e) => {
+    setInput(e.target.value);
   };
 
   return (
@@ -11,9 +17,16 @@ const CreateTodo = () => {
       <h1 style={{ color: "red", fontSize: "45" }} className="btn">
         {count}
       </h1>
+      <input type="text" onChange={handleChange} />
       <button value="Add Task button" onClick={handleClick}>
         Add Task
       </button>
+      <h2>{input}</h2>
+      <ul>
+        {task.map((task) => (
+          <li key={task}>{task}</li>
+        ))}
+      </ul>
     </>
   );
 };
