e5769996e12b46c49ef2c6839429954e4d2596b1
pranavsuryawanshi96 <pranavsuryawanshi61@gmail.com>
2026-05-16 16:35:25 +0530

    useSearchParams

--- Module 6 React Routing/src/components/Articles/Articles.jsx
+++ Module 6 React Routing/src/components/Articles/Articles.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Articles = () => {
+  const [searchParam, setSearchParam] = useSearchParams();
+  const sortBy = searchParam.get("sortBy");
+  const category = searchParam.get("category");
+  const sortByViews = searchParam.get("sortByViews");
+  const handleSortBy = () => {
+    setSearchParam({
+      sortByViews: "Views",
+      category: "category",
+    });
+  };
   return (
     <div>
       <h2>
         Articles
-        <p>SortBy:Category:</p>
+        <p>
+          SortBy: {sortBy ?? sortByViews}Category:{category}
+        </p>
       </h2>
+      <button onClick={handleSortBy}>Sort By Views</button>
     </div>
   );
 };
