deb82203d5d7f2aee3e007ff2dc3e8988302f874
pranavsuryawanshi96 <pranavsuryawanshi61@gmail.com>
2026-05-17 21:50:02 +0530

    string query url

--- Module 6 React Routing/src/components/Articles/Articles.jsx
+++ Module 6 React Routing/src/components/Articles/Articles.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
 
+const dummyArticles = [
+  { id: 1, title: "Laptop", views: 290, category: "electronics" },
+  { id: 2, title: "Smartphone", views: 300, category: "electronics" },
+  { id: 3, title: "Shoes", views: 100, category: "fashion" },
+  { id: 4, title: "washing machine", views: 150, category: "electronics" },
+];
+
 const Articles = () => {
   const [searchParam, setSearchParam] = useSearchParams();
   const sortBy = searchParam.get("sortBy");
   const category = searchParam.get("category");
   const sortByViews = searchParam.get("sortByViews");
+  let filteredArticle = dummyArticles;
   const handleSortBy = () => {
     setSearchParam({
-      sortByViews: "Views",
-      category: "category",
+      sortByViews: "true",
+      category: "electronics",
     });
   };
+
+  if (category) {
+    filteredArticle = filteredArticle.filter(
+      (article) => article.category === category,
+    );
+  }
+
+  if (sortByViews === "true") {
+    filteredArticle = [...filteredArticle].sort((a, b) => a.views - b.views);
+  }
+
   return (
     <div>
       <h2>
@@ -21,6 +40,15 @@ const Articles = () => {
         </p>
       </h2>
       <button onClick={handleSortBy}>Sort By Views</button>
+      <ul>
+        {filteredArticle.map((article) => {
+          return (
+            <li key={article.id}>
+              {article.title}-{article.views} views ({article.category})
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
