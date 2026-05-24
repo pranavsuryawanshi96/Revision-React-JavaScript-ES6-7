d3b02437de8e95976b4b065c146d6462dfc8ff8a
pranavsuryawanshi96 <pranavsuryawanshi61@gmail.com>
2026-05-24 15:32:47 +0530

    Axios method

--- Module 6 React Routing useEffect/src/components/Admin/Seller.jsx
+++ Module 6 React Routing useEffect/src/components/Admin/Seller.jsx
@@ -1,13 +1,23 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Seller = () => {
-  const [name, setName] = useState("");
+  const [users, setUsers] = useState([]);
+  // const [name, setName] = useState("");
+  // useEffect(() => {
+  //   console.log("Component Mount.!!");
+  //   //  demounting or cleanup function
+  //   return () => {
+  //     console.log("Component UnMount.!!");
+  //   };
+  // }, []);
   useEffect(() => {
-    console.log("Component Mount.!!");
-    //  demounting or cleanup function
-    return () => {
-      console.log("Component UnMount.!!");
-    };
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then((res) => res.json())
+    //   .then((data) => console.log(data));
   }, []);
   return (
     <div>
@@ -18,6 +28,9 @@ const Seller = () => {
           setName(e.target.value);
         }}
       />
+      {users.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
     </div>
   );
 };
