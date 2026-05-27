f4931bcd5b0f35c08dca2eb53589b95d08624d9c
pranavsuryawanshi96 <pranavsuryawanshi61@gmail.com>
2026-05-27 22:53:01 +0530

    custom hooks

--- Module 8 React Design Patterns/src/containers/UserContainer.jsx
+++ Module 8 React Design Patterns/src/containers/UserContainer.jsx
@@ -1,31 +1,8 @@
-import React, { useEffect, useState } from "react";
 import UserList from "../components/UserList";
+import useFetch from "../hooks/useFetch";
 
 const UserContainer = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setErrors] = useState(false);
-
-  const getUsers = async () => {
-    // fetch users still loading
-    setIsLoading(true);
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users",
-      );
-      const json = await response.json();
-      if (!json) return;
-      setUsers(json);
-      setIsLoading(false);
-    } catch (error) {
-      setErrors(error);
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getUsers();
-  }, []);
+  const { users, isLoading, error } = useFetch();
   return (
     <div>
       <UserList users={users} loading={isLoading} error={error} />
---
+++ Module 8 React Design Patterns/src/hooks/useFetch.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+export const useFetch = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setErrors] = useState(false);
+
+  const getUsers = async () => {
+    // fetch users still loading
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users",
+      );
+      const json = await response.json();
+      if (!json) return;
+      setUsers(json);
+      setIsLoading(false);
+    } catch (error) {
+      setErrors(error);
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return { users, isLoading, error };
+};
+
+export default useFetch;
